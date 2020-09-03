c11a18e23b52d1cb196ff1f4f03662f2071b1c19
hahnec <inbox@christopherhahne.de>
2020-09-03 03:27:36 +0200

    fix(plt): vertical flip for 3D plot

--- depthy/misc/plots.py
+++ depthy/misc/plots.py
@@ -24,17 +24,21 @@ def plot_point_cloud(disp_arr: np.ndarray,
     if down_scale < 1 or down_scale > min(disp_arr.shape[:2])//2:
         raise IndexError('Downscale factor is %s and out-of-range.' % down_scale)
 
-    fig, ax = plt.figure(), plt.axes(projection='3d') if ax is None else (None, ax)#.set(projection='3d')
-    zz = disp_arr[::down_scale, ::down_scale, ...]
-    xx, yy = np.meshgrid(np.arange(zz.shape[1]), np.arange(zz.shape[0]))
-
+    # rgb image presence/absence handling
     if rgb_img is None or disp_arr.shape[:2] != rgb_img.shape[:2]:
         rgb = np.ones(disp_arr.shape)[::down_scale, ::down_scale, ...]
         if rgb_img is not None:
             warnings.warn('Depth map and RGB image dimension mismatch.')
     else:
-        rgb = rgb_img[::down_scale, ::down_scale, ...]
+        # flip x-axis and downscale rgb image
+        rgb = rgb_img[:, ::-1, ...][::down_scale, ::down_scale, ...]
 
+    # flip x-axis and downscale depth map
+    zz = disp_arr[:, ::-1][::down_scale, ::down_scale, ...]
+    xx, yy = np.meshgrid(np.arange(zz.shape[1]), np.arange(zz.shape[0]))
+
+    # plot depth data
+    fig, ax = plt.figure(), plt.axes(projection='3d') if ax is None else (None, ax)
     ax.scatter(xx, yy, zz, c=rgb.reshape(-1, rgb.shape[-1]) / rgb.max(), s=0.5)
     ax.view_init(view_angles[0], view_angles[1])
 
