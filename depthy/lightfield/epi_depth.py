b4f0f718ccaa93a7979cc72a1eaeb0f7dbb92fc6
hahnec <inbox@christopherhahne.de>
2020-12-10 13:01:06 +0100

    fix(div): skip warning for division by zero confidence

--- depthy/lightfield/epi_depth.py
+++ depthy/lightfield/epi_depth.py
@@ -104,7 +104,8 @@ def coherence_weighting(disp_arr: np.ndarray = None, reli_arr: np.ndarray = None
     """
 
     confidence = np.mean(reli_arr**2, axis=-1)
-    disparity = np.mean(disp_arr*reli_arr**2, axis=-1) / confidence
+    disparity = np.divide(np.mean(disp_arr*reli_arr**2, axis=-1), confidence,
+                          out=np.zeros_like(disp_arr), where=confidence != 0)
     confidence = np.sqrt(confidence)
 
     return disparity, confidence
